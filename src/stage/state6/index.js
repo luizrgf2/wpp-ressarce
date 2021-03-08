335876cea56224b4ea1e07bd2f684f189fa24216
Luiz Ricardo <luizrgfelipe@gmail.com>
2021-03-08 08:30:19 -0300

    implenebtado o nome nos redirects do stage5 e o stage6

--- src/stage/state6/index.js
+++ src/stage/state6/index.js
@@ -21,7 +21,8 @@ async function stage2(client,message,socket){
         if(message.body == '1'){
             await User.updateOne({userid:message.from},{state6:{nome:'op1',state:1}}) // 
             await client.sendText(message.from,dates.stage1) // aguarde um atendente ressarce
-            await Red.create({userid:message.from,red:'Falar com suporte cliente'}) // salva no banco de dados que esta interessado em conversar com um atendente
+            let user = await User.find({userid:message.from})
+            await Red.create({userid:message.from,red:'Falar com suporte cliente',conversas:[],nome:user.nome}) // salva no banco de dados que esta interessado em conversar com um atendente
             
         }
         //redirecionar para um atendente ressarce
@@ -29,7 +30,8 @@ async function stage2(client,message,socket){
 
             await User.updateOne({userid:message.from},{state6:{nome:'op2',state:1}}) // 
             await client.sendText(message.from,dates.stage1) // aguarde um atendente ressarce
-            await Red.create({userid:message.from,red:'Falar com suporte franqueado'}) // salva no banco de dados que esta interessado em conversar com um atendente
+            let user = await User.find({userid:message.from})
+            await Red.create({userid:message.from,red:'Falar com suporte franqueado',conversas:[],nome:user.nome}) // salva no banco de dados que esta interessado em conversar com um atendente
             
 
         }
